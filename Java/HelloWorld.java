bd050b05c28acab4cfd7f7db83183ffeef4c7bb1
SebinYu <101511435+SebinYu@users.noreply.github.com>
2022-04-02 23:45:36 +0900

    Add files via upload

---
+++ Java/HelloWorld.java
@@ -0,0 +1,26 @@
+package first;
+
+public class HelloWorld {
+// 프로그럄의 시작점
+	/**
+	 * 문서화 주석
+	 * 보통 클래스 바로 아래에 작성
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		
+//		System.out.println("행단위 주석 단축키-> Ctrl+/ ");
+		
+//		System.out.println("블렉단위 주석 단축키-> 블럭 드레그 + Ctrl + / ");
+//		System.out.println("주석 해재- >주석문에 Ctrl+/ ");
+		
+/*		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!"); */
+		
+	}
+
+}
